8d9560f31c1baa2c1b689c64794a6d50d480784c
Xair478 <abikaamantaev820@gmail.com>
2023-05-30 22:52:48 +0600

    Project files, starting point
    for freelance task we need to configure basic Core Data entities and attributes. Also we should work with user defaults and connect ui with data storage

---
+++ MyCars/MyCars/ViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ViewController.swift
+//  MyCars
+//
+//  Created by Абылхаир Амантаев on 29.05.2023.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+    
+    var context: NSManagedObjectContext!
+    
+    lazy var dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeStyle = .none
+        return df
+    }()
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var markLabel: UILabel!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var lastTimeStartedLabel: UILabel!
+    @IBOutlet weak var numberOfTripsLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var myChoiceImageView: UIImageView!
+    
+    @IBAction func segmentedCtrlPressed(_ sender: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func startEnginePressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func rateItPressed(_ sender: UIButton) {
+        
+    }
+    
+    private func getDataFromFile() {
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "mark != nil")
+        
+        var records = 0
+        
+        do {
+            records = try context.count(for: fetchRequest)
+            print("Data is there")
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+        guard records == 0 else { return }
+        
+        
+        guard let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist"),
+              let dataArray = NSArray(contentsOfFile: pathToFile) else { return }
+        
+        for dictionary in dataArray {
+            let entity = NSEntityDescription.entity(forEntityName: "Car", in: context)
+            let car = NSManagedObject(entity: entity!, insertInto: context) as? Car
+            
+            let carDictionary = dictionary as! [String : AnyObject]
+            car?.mark = carDictionary["mark"] as? String
+            car?.model = carDictionary["model"] as? String
+            car?.rating = carDictionary["rating"] as! Double
+            car?.lastStarted = carDictionary["lastStarted"] as? Date
+            car?.timesDriven = carDictionary["timesDriven"] as! Int16
+            car?.myChoice = carDictionary["myChoice"] as! Bool
+            
+            let imageName = carDictionary["imageName"] as? String
+            let image = UIImage(named: imageName!)
+            let imageData = image!.pngData()
+            car?.imageData = imageData
+            
+            if let colorDictionary = carDictionary["tintColor"] as? [String : Float] {
+                car?.tintColor = getColor(colorDictionary: colorDictionary)
+            }
+        }
+    }
+    
+    private func getColor(colorDictionary: [String : Float]) -> UIColor {
+        guard let red = colorDictionary["red"],
+              let green = colorDictionary["green"],
+              let blue = colorDictionary["blue"] else { return UIColor() }
+        return UIColor(red: CGFloat(red / 255), green: CGFloat(green / 255), blue: CGFloat(blue / 255), alpha: 1.0)
+    }
+    
+    private func insertDataFrom(selectedCar car: Car) {
+        carImageView.image = UIImage(data: car.imageData!)
+        markLabel.text = car.mark
+        modelLabel.text = car.model
+        myChoiceImageView.isHidden = !(car.myChoice)
+        ratingLabel.text = "Rating: \(car.rating) / 10"
+        numberOfTripsLabel.text = "Number of trips: \(car.timesDriven)"
+        
+        lastTimeStartedLabel.text = "Last time started: \(dateFormatter.string(from: car.lastStarted!))"
+        segmentedControl.tintColor = car.tintColor as? UIColor
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getDataFromFile()
+        
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        let mark = segmentedControl.titleForSegment(at: 0)
+        fetchRequest.predicate = NSPredicate(format: "mark == %@", mark!)
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            let car = results.first
+            insertDataFrom(selectedCar: car!)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+}
+
